77df01936765391da57bc78ab614b33536d27c28
draym <draym@qq.com>
2024-11-05 16:59:23 +0800

    update

--- v2/log/rename.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-for fname in os.listdir('.'):
-    if fname.endswith('.json'):
-        new_name = fname.replace('_K5', '').replace('_dir', '').replace('_undir', '').replace('_adj', '').replace('_adjt', '')
-        os.remove(fname)
